ecb48b90117b4bb6b89b961abec4a105690ba26a
Solomon Oluwatomisola <oluwatomisola@ext.kobo360.com>
2022-12-16 23:44:29 +0100

    video gallery done

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
-const menu = document.querySelector('.menu')
+const menu = document.querySelector('.menu');
+const videos = document.querySelectorAll('.video');
 
 menu.addEventListener('click', () =>{
     document.querySelectorAll('.target').forEach((item) => {
         item.classList.toggle('change')
-    })
-})
+    });
+});
 
 document.querySelectorAll('.wrapper').forEach((item) => {
     item.addEventListener('click', () =>{
         document.querySelectorAll('.target').forEach((item) => {
             item.classList.toggle('change')
-        })
-    })
-})
+        });
+    });
+});
+
+videos.forEach((video) => {
+    video.addEventListener('mouseover', () =>{
+        video.play()
+    });
+    video.addEventListener('mouseout', () =>{
+        video.pause()
+    });
+});
